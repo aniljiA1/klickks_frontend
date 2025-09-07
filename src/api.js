a9bde2d4ec08044ae3d5c508852cf9e96b55751f
aniljiA1 <eeeanilkumar1995@gmail.com>
2025-09-08 00:10:37 +0530

    Updated frontend

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
   withCredentials: true, // needed for cookies/session
 });
 
